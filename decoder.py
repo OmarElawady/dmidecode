f15fbe74134eb248ad429658f19843d47644ed49
Omar Elawady <omarelawady1998@gmail.com>
2019-07-03 16:18:42 +0200

    Fix naming

--- decoder.py
+++ decoder.py
@@ -5,41 +5,46 @@ class Section:
         self.typ = typ
         self.size = size
         self.data = {}
-    def setProperty(self, key, val):
+    def set_property(self, key, val):
         self.data[key] = val
-def countTabs(string):
-    i = 0
-    while i < len(string) and string[i] == '\t':
-        i = i + 1
-    return i
-def parseHandle(handle):
-    return [x.strip() for x in handle.replace('Handle', '').replace('DMI type', '').replace('bytes', '').split(', ')]
-def parseDMI(string):
-    DMIData = {}
-    lasOpenSection = ""
-    variableData = ""
-    variableName = ""
-    for line in string.split('\n')[6:]:
-        tabsCount = countTabs(line)
-        if variableName != "" and tabsCount != 2:
-            DMIData[lastOpenSection].setProperty(variableName, variableData.strip())
-            variableName = variableData = ""
-        if len(line) == 0:
-            continue
-        elif len(line) > 6 and line[0:6] == "Handle":
-          handleData = parseHandle(line)
-        elif tabsCount == 0:
-            DMIData[line] = Section(handleData[0], handleData[1], handleData[2])
-            lastOpenSection = line
-        elif tabsCount == 1:
-            splitted = line.split(':')
-            variableName = splitted[0].strip()
-            variableData = splitted[1].strip() + "\n"
-        else:
-            variableData += line[2:] + "\n"
-    return DMIData
+
+class DMIParser:
+    def count_tabs(self, string):
+        i = 0
+        while i < len(string) and string[i] == '\t':
+            i = i + 1
+        return i
+        
+    def parse_handle(self, handle):
+        return [x.strip() for x in handle.replace('Handle', '').replace('DMI type', '').replace('bytes', '').split(', ')]
+
+    def parse_dmi(self, string):
+        DMIData = {}
+        lasOpenSection = ""
+        variableData = ""
+        variableName = ""
+        for line in string.split('\n')[6:]:
+            tabs_count = self.count_tabs(line)
+            
+            if variableName != "" and tabs_count != 2:
+                DMIData[lastOpenSection].set_property(variableName, variableData.strip())
+                variableName = variableData = ""
+            if len(line) == 0:
+                continue
+            elif len(line) > 6 and line[0:6] == "Handle":
+              handleData = self.parse_handle(line)
+            elif tabs_count == 0:
+                DMIData[line] = Section(handleData[0], handleData[1], handleData[2])
+                lastOpenSection = line
+            elif tabs_count == 1:
+                splitted = line.split(':')
+                variableName = splitted[0].strip()
+                variableData = splitted[1].strip() + "\n"
+            else:
+                variableData += line[2:] + "\n"
+        return DMIData
 
 if __name__ == "__main__":
     data = open("data.txt", "r").read()
-    parsed = parseDMI(data)
+    parsed = DMIParser().parse_dmi(data)
     print(json.dumps(parsed, default=lambda o: o.__dict__, indent=2))
